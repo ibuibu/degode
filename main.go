6816cada49728e5df37a99f1a0a7cdd5fb8b1ad0
ibuibu <ibuibu69@gmail.com>
2021-02-02 22:52:16 +0900

    Formated

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
+	"fmt"
+	"net/url"
 	"os"
-  "net/url"
 )
 
 func main() {
-  fmt.Println("===Input Percent Encoded URL!===")
-  stdin := bufio.NewScanner(os.Stdin)
-  stdin.Scan()
-  input := stdin.Text()
-  out, _ := url.QueryUnescape(input)
-  fmt.Println("")
-  fmt.Println("===Here you are!===")
-  fmt.Println(out)
+	fmt.Println("===Input Percent Encoded URL!===")
+	stdin := bufio.NewScanner(os.Stdin)
+	stdin.Scan()
+	input := stdin.Text()
+	out, _ := url.QueryUnescape(input)
+	fmt.Println("")
+	fmt.Println("===Here you are!===")
+	fmt.Println(out)
 }
