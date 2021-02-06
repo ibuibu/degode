418a70d7d739e6221a1f2ecfe9476b25e03d4fc7
Kenta Katsura <katsura.rs@gmail.com>
2021-02-06 23:11:04 +0900

    Implemented read from pipe

--- main.go
+++ main.go
@@ -3,11 +3,39 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"net/url"
 	"os"
 )
 
 func main() {
+	fi, err := os.Stdin.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	if fi.Mode()&os.ModeNamedPipe != 0 {
+		readByPipe()
+	} else {
+		readByInteractive()
+	}
+}
+
+func readByPipe() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil && err == io.EOF {
+			break
+		}
+
+		decoded, _ := url.QueryUnescape(string(line))
+		fmt.Println(decoded)
+	}
+}
+
+func readByInteractive() {
 	fmt.Println("===Input Percent Encoded URL!===")
 	stdin := bufio.NewScanner(os.Stdin)
 	stdin.Scan()
